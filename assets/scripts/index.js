f4df516ab0857c798964d3609052726e6d8ff232
JenWeber <weberj10@gmail.com>
2016-01-31 16:39:57 -0500

    disallow overwriting a past move

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -29,7 +29,8 @@ var turnCounter = 0;
 var clearBoard = function() {
     $(".square").text("");
 };
-
+//calls clear the board - remove after testing
+clearBoard();
 //Each session keeps a running turnCounter. If it's even, it's x's turn, and if odd, O's turn. This means that turns will always alternate, regardless of win/lose/draw since the board has an odd number of squares. CHANGE VAR TO CONST
 var whoseTurn = function() {
     return turnCounter%2 === 0 ? "X" : "O";
@@ -71,72 +72,22 @@ var gameResult = function () {
     columnWin(boardArray);
     diagonalWin(boardArray);
     turnCounter +=1;
-    console.log(boardArray);
+};
+// check to see if a move has already been made in this square
+var validMove = function(moveAttempt) {
+  if ((moveAttempt !== "O") & (moveAttempt !== "X")) {
+    return true;
+  } else {
+    return false;
+  }
 };
 // basic function to change the value of a square
 $(".square").on("click", function() {
-  $(this).text(whoseTurn());
-  gameResult();
+  if (validMove($(this).text()) === true) {
+    $(this).text(whoseTurn());
+    gameResult();
+  }
 });
 
+
 //NEED TO ADD preventing someone from changing a box that already has a value --- if statement in makeMove? Need to add checking for a draw. Need to add some change to the html that will show whose turn it is. Need to add win/loss/draw counter
-
-
-//CODE BELOW is plain JS. Code above incorporates JQuery
-//establish game board
-// var board = [['', '', ''], ['', '', ''], ['', '', '']];
-//
-// //establish turn counter
-// var turnCounter = 0;
-//
-// //CHANGE VAR TO CONST
-// var clearBoard = function() {
-//     board = [['', '', ''], ['', '', ''], ['', '', '']];
-// };
-//
-// //Each session keeps a running turnCounter. If it's even, it's x's turn, and if odd, O's turn. This means that turns will always alternate, regardless of win/lose/draw since the board has an odd number of squares. CHANGE VAR TO CONST
-// var whoseTurn = function() {
-//     return turnCounter%2 === 0 ? "X" : "O";
-// };
-//
-// //check to see if there are 3 in a row. Runs after every makeMove. REPLACE CONSOLE LOG WITH WINNER DISPLAY IN HTML
-// var rowWin = function(){
-//     var column = 0;
-//     for (var row = 0; row < 3; row++) {
-//         if (((board[row][column] === board[row][column+1]) && (board[row][column+1] === board[row][column+2])) && board[row][column] !== '') {
-//             return console.log(board[row][column] + ' is the winner!');
-//         }
-//     }
-// };
-// //check to see if there are 3 in a column. Runs after every makeMove. REPLACE CONSOLE LOG WITH WINNER DISPLAY IN HTML
-// var columnWin = function(){
-//     var row = 0;
-//     for (var column = 0; column < 3; column++) {
-//         if (((board[row][column] === board[row+1][column]) && (board[row+1][column] === board[row+2][column])) && board[row][column] !== '') {
-//             return console.log(board[row][column] + ' is the winner!');
-//         }
-//     }
-// };
-// //check for diagonal wins. Runs after every makeMove. REPLACE CONSOLE LOG WITH WINNER DISPLAY IN HTML
-// var diagonalWin = function(){
-//     var row = 0;
-//     var column = 0;
-//     if (((board[row][column] === board[row+1][column+1]) && (board[row+1][column+1] === board[row+2][column+2])) && board[row+1][column+1] !== '') {
-//             return console.log(board[row+1][column+1] + ' is the winner!');
-//         } else if (((board[row][column+2] === board[row+1][column+1]) && (board[row+1][column+1] === board[row+2][column])) && board[row+1][column+1] !== '') {
-//             return console.log(board[row+1][column+1] + ' is the winner!');
-//         }
-// };
-// var checkForWinner = function () {
-//     rowWin();
-//     columnWin();
-//     diagonalWin();
-//     turnCounter +=1;
-//     console.log(board);
-// };
-// //NEED TO ADD preventing someone from changing a box that already has a value --- if statement in makeMove?
-// //how to connect this with the html gameboard?
-// var makeMove = function(row,column) {
-//     board[row][column] = whoseTurn();
-//     checkForWinner();
-// };
