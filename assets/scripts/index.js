8a1f8b83f001592b16544b47b446bb73239bd3a3
JenWeber <weberj10@gmail.com>
2016-01-31 18:36:36 -0500

    add draw as a game-ending condition

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -22,15 +22,21 @@ var getBoard = function () {
 };
 //establish initial board
 var boardArray = getBoard();
+
 //establish turn counter
 var turnCounter = 0;
+
+// gameStatus will toggle between active and inactive. Game becomes inactive upon a win/loss
+var gameStatus = "active";
 
 //clear the game board for a new round. $square refers to ALL squares
 var clearBoard = function() {
     $(".square").text("");
 };
+
 //calls clear the board - remove after testing
 clearBoard();
+
 //Each session keeps a running turnCounter. If it's even, it's x's turn, and if odd, O's turn. This means that turns will always alternate, regardless of win/lose/draw since the board has an odd number of squares. CHANGE VAR TO CONST
 var whoseTurn = function() {
     return turnCounter%2 === 0 ? "X" : "O";
@@ -42,45 +48,69 @@ var rowWin = function(currentBoard){
     var column = 0;
     for (var row = 0; row < 3; row++) {
         if (((currentBoard[row][column] === currentBoard[row][column+1]) && (currentBoard[row][column+1] === currentBoard[row][column+2])) && currentBoard[row][column] !== '') {
+            gameStatus = "inactive";
             return console.log(currentBoard[row][column] + ' is the winner!');
         }
     }
 };
+
 //check to see if there are 3 in a column. Runs after every makeMove. REPLACE CONSOLE LOG WITH WINNER DISPLAY IN HTML
 var columnWin = function(currentBoard){
     var row = 0;
     for (var column = 0; column < 3; column++) {
         if (((currentBoard[row][column] === currentBoard[row+1][column]) && (currentBoard[row+1][column] === currentBoard[row+2][column])) && currentBoard[row][column] !== '') {
+            gameStatus = "inactive";
             return console.log(currentBoard[row][column] + ' is the winner!');
         }
     }
 };
+
 //check for diagonal wins. Runs after every makeMove. REPLACE CONSOLE LOG WITH WINNER DISPLAY IN HTML
-var diagonalWin = function(currentBoard){
+var diagonalWin = function(currentBoard) {
     var row = 0;
     var column = 0;
     if (((currentBoard[row][column] === currentBoard[row+1][column+1]) && (currentBoard[row+1][column+1] === currentBoard[row+2][column+2])) && currentBoard[row+1][column+1] !== '') {
+            gameStatus = "inactive";
             return console.log(currentBoard[row+1][column+1] + ' is the winner!');
         } else if (((currentBoard[row][column+2] === currentBoard[row+1][column+1]) && (currentBoard[row+1][column+1] === currentBoard[row+2][column])) && currentBoard[row+1][column+1] !== '') {
+            gameStatus = "inactive";
             return console.log(currentBoard[row+1][column+1] + ' is the winner!');
         }
 };
+var draw = function(currentBoard) {
+  var isDraw = true;
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      if (currentBoard[i][j] === "") {
+        isDraw = false;
+      }
+    }
+  }
+  if (isDraw === true) {
+    gameStatus = "inactive";
+    console.log("the game is a draw");
+  }
+};
+
 //gameResult updates boardArray with the current game status, then checks to see if any conditions exist which would end the game. The turn counter is advanced by 1 so that it becomes the other person's turn
 var gameResult = function () {
   var boardArray = getBoard();
     rowWin(boardArray);
     columnWin(boardArray);
     diagonalWin(boardArray);
+    draw(boardArray);
     turnCounter +=1;
 };
+
 // check to see if a move has already been made in this square
 var validMove = function(moveAttempt) {
-  if ((moveAttempt !== "O") & (moveAttempt !== "X")) {
+  if ((moveAttempt !== "O") && (moveAttempt !== "X") && (gameStatus === "active")) {
     return true;
   } else {
     return false;
   }
 };
+
 // basic function to change the value of a square
 $(".square").on("click", function() {
   if (validMove($(this).text()) === true) {
@@ -90,4 +120,9 @@ $(".square").on("click", function() {
 });
 
 
-//NEED TO ADD preventing someone from changing a box that already has a value --- if statement in makeMove? Need to add checking for a draw. Need to add some change to the html that will show whose turn it is. Need to add win/loss/draw counter
+
+
+//Need to add checking for a draw.
+//Need to add some change to the html that will show whose turn it is.
+//Need to add win/loss/draw counter
+//Need to add new game functionality
